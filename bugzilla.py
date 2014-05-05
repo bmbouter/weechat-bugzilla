54bda9aaaf3d92a14396868ba42d18eebcc72028
Brian Bouterse <bmbouter@gmail.com>
2014-05-05 13:22:28 -0400

    Removing unecessary message filtering on hook_print

--- bugzilla.py
+++ bugzilla.py
@@ -10,25 +10,19 @@ SCRIPT_AUTHOR = 'Wraithan <xwraithanx@gmail.com>'
 SCRIPT_VERSION = '0.0.2'
 SCRIPT_LICENSE = 'MIT'
 SCRIPT_DESC = "Create bugzilla links from 'bz <number>'"
-SCRIPT_COLOR = 'yellow'
+SCRIPT_COLOR = 'black'
 
 settings = {}
 
-if weechat.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE,
-                    SCRIPT_DESC, '', ''):
-
+if weechat.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE, SCRIPT_DESC, '', ''):
     for option, default_value in settings.iteritems():
         if weechat.config_get_plugin(option) == '':
             weechat.config_set_plugin(option, default_value)
 
-    hooks = ('notify_message', 'notify_private', 'notify_highlight')
-
-    for hook in hooks:
-        weechat.hook_print('', hook, 'bz', 1, 'hook_print_callback', '')
+    weechat.hook_print('', '', 'bz', 1, 'hook_print_callback', '')
 
 
-def hook_print_callback(data, buffer, date, tags, displayed, highlight, prefix,
-                        message):
+def hook_print_callback(data, buffer, date, tags, displayed, highlight, prefix, message):
     url = '{0}[https://bugzilla.redhat.com/show_bug.cgi?id={1}]'
     for number in re.findall(r'[bB][zZ] ?#?(\d+)', message):
         weechat.prnt(buffer, url.format(weechat.color(SCRIPT_COLOR), number))
